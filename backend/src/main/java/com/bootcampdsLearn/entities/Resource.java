d9d4d4ce5f360bb75aae3655217ead96ba14e266
RanyellHenrique <ranyellhenrique@gmail.com>
2020-11-20 18:20:05 -0300

    Section

--- backend/src/main/java/com/bootcampdsLearn/entities/Resource.java
+++ backend/src/main/java/com/bootcampdsLearn/entities/Resource.java
@@ -1,6 +1,8 @@
 package com.bootcampdsLearn.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.bootcampdsLearn.entities.enums.ResourceType;
 
@@ -29,6 +32,9 @@ public class Resource implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "offer_id")
 	private Offer offer;
+	
+	@OneToMany(mappedBy = "resource")
+	private List<Section> sections = new ArrayList<>();
 	
 	public Resource() {
 		
@@ -109,6 +115,10 @@ public class Resource implements Serializable {
 
 	public void setOffer(Offer offer) {
 		this.offer = offer;
+	}
+	
+	public List<Section> getSections() {
+		return sections;
 	}
 
 	@Override
